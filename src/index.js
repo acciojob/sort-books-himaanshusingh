cf5ba88fb2e1b5a0e63c4590393350b13ab0668c
Himanshu Singh <himanshusingh1k@gmail.com>
2026-02-17 09:38:11 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import "regenerator-runtime/runtime";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
